67cbe1b19b2f9465c5ef88969a74d03ff3f81e36
土居健太郎 <kentarou9.60@icloud.com>
2025-01-27 18:37:13 +0900

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Todo from "./components/Todo";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
 
 function App(props) {
-  const [count, setCount] = useState(0)
 
-  console.log('props:', props)
+    const [tasks, setTasks] = useState(props.tasks);
 
-  return (
-    <>
-      <header>
-        <h1>Hello {props.subject}!</h1>
-        <button type="button" className="primary">
-          Click me!!
-        </button>
-      </header>
-    </>
-  )
+    function addTask(name) {
+        const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+        setTasks([...tasks, newTask]);
+    }
+
+    const taskList = tasks?.map((task) =>
+        <Todo
+            id={task.id}
+            name={task.name}
+            completed={task.completed}
+            key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
+            deleteTask={deleteTask}
+        />
+        
+    )
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            // このタスクが編集されたタスクと同じ ID を持っている場合
+            if (id === task.id) {
+                // オブジェクトを開いて、 `completed` プロップが
+                // 反転された新しいオブジェクトを作成します。
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter((task) => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
+
+
+    const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
+        
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form addTask={addTask} />
+            <div className="filters btn-group stack-exception">
+                <FilterButton />
+                <FilterButton />
+                <FilterButton />
+            </div>
+            <h2 id="list-heading">{headingText}</h2>
+            <ul
+                role="list"
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading">
+                {taskList}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
